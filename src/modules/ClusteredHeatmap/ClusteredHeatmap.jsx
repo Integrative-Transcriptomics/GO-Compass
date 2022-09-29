40a6cc0305f798b344f1442e5a4cb29ae2171d89
ThHarbig <theresa_ha@hotmail.de>
2022-09-29 09:13:40 +0200

    remove warnings

--- src/modules/ClusteredHeatmap/ClusteredHeatmap.jsx
+++ src/modules/ClusteredHeatmap/ClusteredHeatmap.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import {inject, observer} from "mobx-react";
 import PropTypes from "prop-types";
 import Tree from "./Tree";
