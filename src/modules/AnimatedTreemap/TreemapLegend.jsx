5be88b9c30f889b584edb64ac5088984a3247ad2
ThHarbig <theresa_ha@hotmail.de>
2024-04-03 15:42:06 +0200

    fix treemap legend when no gene data is displayed

--- src/modules/AnimatedTreemap/TreemapLegend.jsx
+++ src/modules/AnimatedTreemap/TreemapLegend.jsx
@@ -11,17 +11,22 @@ const TreemapLegend = (props) => {
 
     return <svg width={320} height={50}>
         <g transform={"translate(0,10)"}>
-            <GradientLegend
-                domain={props.glyphEncoding === "updown" ? props.foregroundScale.domain().map(d => d * 100) : props.foregroundScale.domain()}
-                range={props.foregroundScale.range()} label={foreGroundLabel}/>
-            <g transform={"translate(" + 110 + ",0)"}>
-                <text fontSize={12}>0</text>
-                <GeneGlyph height={20} width={100} sigWidth={50} backgroundColor={"grey"} foregroundColor={"red"}/>
-                <text x={80} fontSize={12}>100</text>
-                <text y={32} fontSize={12}>% present</text>
-            </g>
-            <polygon points={"100,0 110,7 110,13 100,20"} fill={"red"} opacity={0.5}/>
-            <polygon points={"210,7 220,0 220,20 210,13"} fill={"grey"} opacity={0.5}/>
+            {props.foregroundScale ?
+                <g>
+                    <GradientLegend
+                        domain={props.glyphEncoding === "updown" ? props.foregroundScale.domain().map(d => d * 100) : props.foregroundScale.domain()}
+                        range={props.foregroundScale.range()} label={foreGroundLabel}/>
+                    <g transform={"translate(" + 110 + ",0)"}>
+                        <text fontSize={12}>0</text>
+                        <GeneGlyph height={20} width={100} sigWidth={50} backgroundColor={"grey"}
+                                   foregroundColor={"red"}/>
+                        <text x={80} fontSize={12}>100</text>
+                        <text y={32} fontSize={12}>% present</text>
+                    </g>
+                    <polygon points={"100,0 110,7 110,13 100,20"} fill={"red"} opacity={0.5}/>
+                    <polygon points={"210,7 220,0 220,20 210,13"} fill={"grey"} opacity={0.5}/>
+                </g>
+                : null}
             <g transform={"translate(" + 220 + ",0)"}>
                 <GradientLegend domain={props.backgroundScale.domain()} range={props.backgroundScale.range()}
                                 label={"Set size"}/>
@@ -30,7 +35,7 @@ const TreemapLegend = (props) => {
     </svg>
 }
 TreemapLegend.propTypes = {
-    foregroundScale: PropTypes.func.isRequired,
+    foregroundScale: PropTypes.func,
     backgroundScale: PropTypes.func.isRequired,
     glyphEncoding: PropTypes.string.isRequired,
 };
